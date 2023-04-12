562c43df4d2d8e2949c19201734c2fcc36198c6b
IrwanFicoFar <irwanhadi9@gmail.com>
2023-04-12 15:55:10 +0700

    restriction navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { ButtonBorrow, ButtonUnavailable } from "../components/Button";
 const Home: FC = () => {
   const [state1, setState] = useState<boolean>(true);
   const [state2, set2State] = useState<boolean>(false);
+
   return (
     <Layout>
       <div className="py-10 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 sm:gap-6 md:gap-8 xl:gap-10 p-4 sm:p-6 md:-8 xl:p-10">
