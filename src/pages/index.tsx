01da29433157b9ecc3bfd6a5aa523c17b7417e9d
IrwanFicoFar <irwanhadi9@gmail.com>
2023-04-12 21:00:42 +0700

    success get and save local storage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect, useState, FormEvent } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 
 import { Layout } from "../components/Layout";
 import { CardLanding } from "../components/Card";
@@ -8,6 +9,7 @@ import { ButtonBorrow, ButtonUnavailable } from "../components/Button";
 
 interface DataType {
   title: string;
+  bookImage: string;
   status: boolean;
   username: string;
 }
@@ -19,25 +21,17 @@ const Home: FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [username, setUsername] = useState<string>("");
 
-  // const params = useParams();
-
   useEffect(() => {
     fetchAllBook();
   }, []);
 
-  // const { username } = params;
-
-  // console.log(username);
-
   const fetchAllBook = () => {
     axios
-      .get("books")
+      .get("/books")
       .then((response) => {
         const { data } = response.data;
-        // console.log(data);
         setDatas(data);
         setUsername(data[0].username);
-        // console.log(data[0].username);
       })
       .catch((error) => {
         const { message } = error.message;
@@ -48,15 +42,21 @@ const Home: FC = () => {
       });
   };
 
-  const saveLocalStorage = (event: FormEvent<HTMLFormElement>) => {
+  const saveLocalStorage = (event: FormEvent<HTMLButtonElement>) => {
     event.preventDefault();
     const userData = { datas };
     const uniqueKey = Date.now();
     console.log(userData, uniqueKey);
     localStorage.setItem(uniqueKey.toString(), JSON.stringify(userData));
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "the book has been saved to Cart",
+      showConfirmButton: false,
+      timer: 1500,
+    });
   };
 
-  // console.log(datas);
   return (
     <Layout>
       {loading ? (
@@ -65,6 +65,7 @@ const Home: FC = () => {
         <div className="py-10 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 sm:gap-6 md:gap-8 xl:gap-10 p-4 sm:p-6 md:-8 xl:p-10">
           {datas.map((data) => (
             <CardLanding
+              BookImage={data.bookImage}
               MyLink={`/detail-book/${data.username}`}
               Title={data.title}
               Owner={data.username}
