49bfd48d4ca758774076d21dd30c6082fe398c95
IrwanFicoFar <irwanhadi9@gmail.com>
2023-04-14 00:21:38 +0700

    edit detail book

--- src/pages/DetailBook.tsx
+++ src/pages/DetailBook.tsx
@@ -45,13 +45,24 @@ const UploadBook: FC = () => {
     setBookSubmit(temp);
   }
 
-  function handleSubmitBook(event: FormEvent<HTMLFormElement>) {
+  function handleSubmitBook(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
+
     const formData = new FormData();
+
+    if (formData === undefined) {
+      Swal.fire({
+        icon: "warning",
+        title: "Please fill edit min 1 ",
+      });
+      return;
+    }
+
     let key: keyof typeof bookSubmit;
     for (key in bookSubmit) {
       formData.append(key, bookSubmit[key] as string);
     }
+    console.log(formData);
     axios
       .put("/books/booksID", formData, {
         headers: {
@@ -62,15 +73,23 @@ const UploadBook: FC = () => {
       .then((response) => {
         const { message } = response.data;
         Swal.fire({
+          icon: "success",
           title: "Success",
           text: message,
           showCancelButton: false,
+          showConfirmButton: true,
+        }).then((result) => {
+          if (result.isConfirmed) {
+            setBookSubmit({});
+            window.location.reload();
+            handleChangeEdithFalse();
+          }
         });
-        setBookSubmit({});
       })
       .catch((error) => {
         const { data } = error.response;
         Swal.fire({
+          icon: "error",
           title: "Failed",
           text: data.message,
           showCancelButton: false,
@@ -79,28 +98,43 @@ const UploadBook: FC = () => {
       .finally(() => fetchDetailBook());
   }
   const handleDeleteBook = () => {
-    axios
-      .delete("books/booksID", {
-        headers: {
-          Authorization: "",
-        },
-      })
-      .then((response) => {
-        const { message } = response.data;
-        Swal.fire({
-          title: "Success",
-          text: message,
-          showCancelButton: false,
-        });
-      })
-      .catch((error) => {
-        const { data } = error.response;
-        Swal.fire({
-          title: "Failed",
-          text: data.message,
-          showCancelButton: false,
-        });
-      });
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios
+          .delete("books/booksID", {
+            headers: {
+              Authorization: "",
+            },
+          })
+          .then((response) => {
+            const { message } = response.data;
+            Swal.fire("Deleted!", "Your file has been deleted.", "success");
+          })
+          .catch((error) => {
+            const { data } = error.response;
+            Swal.fire({
+              title: "Failed",
+              text: data.message,
+              showCancelButton: false,
+            });
+          });
+      }
+    });
+  };
+
+  const handleChangeEdith = () => {
+    setIsEdit(true);
+  };
+  const handleChangeEdithFalse = () => {
+    setIsEdit(false);
   };
 
   return (
@@ -134,6 +168,7 @@ const UploadBook: FC = () => {
                       type="button"
                       className="py-2 px-4 m-2 w-full inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-@2A9D8F text-white hover:bg-@1F7168 focus:outline-none   transition-all text-sm dark:focus:ring-offset-gray-800"
                       data-hs-overlay="#hs-medium-modal"
+                      onClick={() => handleChangeEdith()}
                     >
                       Edit Book
                     </button>
@@ -144,23 +179,20 @@ const UploadBook: FC = () => {
                     <button
                       type="button"
                       className="py-2 px-4 m-2 w-full inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-@E76F51 text-white hover:bg-@F4A261 focus:outline-none   transition-all text-sm dark:focus:ring-offset-gray-800"
+                      onClick={() => handleDeleteBook()}
                     >
                       Delete
                     </button>
                   ) : (
                     <></>
                   )}
-
-                  <div
-                    id="hs-medium-modal"
-                    className="hs-overlay hidden w-full h-full fixed top-0 left-0 z-[60] overflow-x-hidden overflow-y-auto"
-                  >
-                    <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all md:max-w-2xl md:w-full m-3 md:mx-auto">
-                      {!isEdit ? (
-                        <form
-                          className="flex flex-col p-5 items-center shadow-lg bg-white gap-3 rounded-large"
-                          onSubmit={(event) => handleSubmitBook(event)}
-                        >
+                  {isEdit && (
+                    <div
+                      id="hs-medium-modal"
+                      className="hs-overlay hidden w-full h-full fixed top-0 left-0 z-[60] overflow-x-hidden overflow-y-auto"
+                    >
+                      <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all md:max-w-2xl md:w-full m-3 md:mx-auto">
+                        <form className="flex flex-col p-5 items-center shadow-lg bg-white gap-3 rounded-large">
                           <h1 className="uppercase font-bold text-3xl text-back dark:text-white">
                             Update Books
                           </h1>
@@ -180,13 +212,7 @@ const UploadBook: FC = () => {
                                   </span>
                                   <Input
                                     type="file"
-                                    className=" w-fit block text-sm text-gray-500
-      file:mr-4 file:py-2 file:px-4
-      file:rounded-md file:border-0
-      file:text-sm file:font-semibold
-      file:bg-@2A9D8F file:text-white
-      hover:file:bg-@1F7168
-    "
+                                    className=" w-fit block text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-@2A9D8F file:text-white hover:file:bg-@1F7168"
                                     onChange={(event) => {
                                       if (!event.currentTarget.files) {
                                         return;
@@ -234,21 +260,22 @@ const UploadBook: FC = () => {
                                 />
                               </div>
                               <div className="w-fit">
-                                <button className="py-1 px-2 m-1 justify-center items-center gap-2 rounded-md border text-lg bg-@2A9D8F text-white font-semibold shadow-sm align-middle hover:scale-105 focus:outline-none   transition-all text-md dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800">
+                                <button
+                                  type="submit"
+                                  className="py-1 px-2 m-1 justify-center items-center gap-2 rounded-md border text-lg bg-@2A9D8F text-white font-semibold shadow-sm align-middle hover:scale-105 focus:outline-none   transition-all text-md dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
+                                  onClick={(event) => {
+                                    handleSubmitBook(event);
+                                  }}
+                                >
                                   Update
                                 </button>
                               </div>
                             </div>
                           </div>
                         </form>
-                      ) : (
-                        <div>
-                          <p>{data.title}</p>
-                          <p>{data.description}</p>
-                        </div>
-                      )}
+                      </div>
                     </div>
-                  </div>
+                  )}
                 </div>
               </div>
             </div>
